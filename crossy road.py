44476f6bdab42b204f5683c1b062a53ef314b1d5
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-05-11 16:57:32 -0700

    Add files via upload

--- crossy road.py
+++ crossy road.py
@@ -1,8 +1,16 @@
 import time, random
 from random import randint
-#name = input('enter your name ')
-def printlanes(content1, content2, content3, content4, content5, content6):
-    print('-'*125),print(content6), print(content6), print(('-'*125)+'\n\n'+ ('-'*125)),print(content5), print(content5), print(('-'*125)+'\n\n'+ ('-'*125)),print(content4), print(content4), print(('-'*125)+'\n\n'+ ('-'*125)),print(content3), print(content3),print(('-'*125)+'\n\n'+ ('-'*125)),print(content2), print(content2), print(('-'*125)+'\n\n'+ ('-'*125)),print(content1), print(content1), print('-'*125)
+def clearscreen(): 
+    print('\n' * 50)
+clearscreen()
+print('Crossy Road')
+print('___________\n\n')
+print('How to play')
+print(' 1. Press control+c to move foreward')
+print(' 2. When you hit an obstacle the game will end\n\n')
+name = input('enter your name ')
+def printlanes(content1, content2, content3,b1,b2):
+    print('-'*125,'\n\n'), print('-'*125,'\n\n'), print('-'*125),print(content3), print(content3),print('-'*125),print(b2),print(b2),print('-'*125),print(content2), print(content2), print('-'*125),print(b1),print(b1),print('-'*125),print(content1), print(content1), print('-'*125)
 def refreshlanecontent(newcontent, lanecontent):
     global lost
     numtoadd = randint(0,2)
@@ -25,64 +33,67 @@ def addguytolanecontent(lanecontent):
     final = firstpart + '(--)' + secondpart
     return final
 print(12 * ('\n\n'+('-'*125)))
-inlane = 1
+inlane = 0
+one = True
 lost = False
 spaces = str(' '*125)
 counter = 0
+b1,b2,b3 = str(' '*65), str(' '*65), str(' '*65)
 lane1, lane2, lane3, lane4, lane5, lane6 =  spaces, spaces, spaces, spaces, spaces, spaces
-printlanes(lane1,lane2,lane3,lane4,lane5,lane6)
+printlanes(lane1,lane2,lane3,'','')
 #randomly generating part ahead
 while True:
     try:
-        randomizer = randint(1,7)
-        add1,add2,add3,add4,add5,add6 = ' ',' ',' ',' ',' ',' '
+        randomizer = randint(1,6)
+        add1,add2,add3,add4,add5 = ' ',' ',' ',' ',' '
         if randomizer == 1:
-            add1 = '<'
+            add1 = '='
         if randomizer == 2:
-            add2 = '#'
+            add2 = '?'
         if randomizer == 3:
             add3 = '$'
         if randomizer == 4:
             add4 = '>'
-        if randomizer == 5:
-            add5 = '?'
-        if randomizer == 6:
-            add6 = '='
-        lane1, lane2, lane3, lane4, lane5, lane6 = refreshlanecontent(add1,lane1), refreshlanecontent(add2,lane2), refreshlanecontent(add3,lane3), refreshlanecontent(add4,lane4), refreshlanecontent(add5,lane5), refreshlanecontent(add6,lane6)
-        if counter >= 64:
-            printlanes(lane1,lane2,lane3,lane4,lane5,lane6)
-            if inlane == 1: 
+        lane1, lane2, lane3, lane4, lane5 = refreshlanecontent(add1,lane1), refreshlanecontent(add2,lane2), refreshlanecontent(add3,lane3), refreshlanecontent(add4,lane4), refreshlanecontent(add5,lane5)
+        if counter >= 60:
+            printlanes(lane1,lane2,lane3,b1,b2)
+            if inlane == 0: 
                 print(' ' * 65, ' (--)\n',' ' * 65,'(--)')
             else:
                 print('\n')
-            time.sleep(0.15)
+            time.sleep(0.25)
         if lost == True:
             break
-        counter += 3
+        counter += 1
     except(KeyboardInterrupt, SystemExit):
         #move foreward
-        if inlane == 6:
-            inlane += 1
-            lane6 = addguytolanecontent(lane6)
-            lane5 = removeguyfromlane(lane5)
-        if inlane == 5:
-            inlane += 1
-            lane5 = addguytolanecontent(lane5)
-            lane4 = removeguyfromlane(lane4)
-        if inlane == 4:
-            inlane += 1
-            lane4 = addguytolanecontent(lane4)
-            lane3 = removeguyfromlane(lane3)
-        if inlane == 3:
-            inlane += 1
-            lane3 = addguytolanecontent(lane3)
-            lane2 = removeguyfromlane(lane2)
-        if inlane == 2:
-            inlane += 1
-            lane2 = addguytolanecontent(lane2)
-            lane1 = removeguyfromlane(lane1)
-        if inlane == 1:
+        if inlane == 0:
             inlane += 1
             lane1 = addguytolanecontent(lane1)
+        elif inlane == 1:
+            inlane += 1
+            lane1 = removeguyfromlane(lane1)
+            b1 = addguytolanecontent(b1)
+        elif inlane == 2:
+            inlane += 1
+            b1 = removeguyfromlane(b1)
+            lane2 = addguytolanecontent(lane2)
+        elif inlane == 3:
+            inlane += 1
+            lane2 = removeguyfromlane(lane2)
+            b2 = addguytolanecontent(b2)
+        elif inlane == 4:
+            inlane += 1
+            b2 = removeguyfromlane(b2)
+            lane3 = addguytolanecontent(lane3)
+        elif inlane == 5:
+            inlane += 1
+            lane3 = removeguyfromlane(lane3)
+            b3 = addguytolanecontent(b3)
+        elif inlane == 6:
+            inlane += 1
+            b3 = removeguyfromlane(b3)
+            lane4 = addguytolanecontent(lane4)
+            
 print('you have lost')
 #highscore board
